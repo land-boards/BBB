81bc4809e18deb683800f90195f7fc020fca46a3
root <root@BBB01.(none)>
2014-07-12 13:20:45 +0000

    stuff

--- blink/blink/blink.py
+++ blink/blink/blink.py
@@ -1,10 +1,25 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-GPIO.setup("P8_10", GPIO.OUT)
+J37 = "GPIO1_29"
+J35 = "GPIO1_0"
+J33 = "GPIO1_1"
+J31 = "GPIO1_4"
+J15 = "GPIO1_28"
+J14 = "GPIO1_16"
+
+GPIO.setup(J37, GPIO.OUT)
+GPIO.setup(J15, GPIO.OUT)
+GPIO.setup(J14, GPIO.OUT)
 
 while True:
-	GPIO.output("P8_10", GPIO.HIGH)
+	GPIO.output(J37, GPIO.HIGH)
 	time.sleep(0.5)
-	GPIO.output("P8_10", GPIO.LOW)
+	GPIO.output(J37, GPIO.LOW)
+	GPIO.output(J15, GPIO.HIGH)
 	time.sleep(0.5)
+	GPIO.output(J15, GPIO.LOW)
+	GPIO.output(J14, GPIO.HIGH)
+	time.sleep(0.5)
+	GPIO.output(J14, GPIO.LOW)
+
