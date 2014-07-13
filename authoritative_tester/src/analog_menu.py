a7d66997e2acb57739cdb0ca6379c9433643f15e
Michael J. Gilliland <mjg.py3@gmail.com>
2014-07-13 16:08:36 -0400

    Sorted Jacks in analog

--- authoritative_tester/src/analog_menu.py
+++ authoritative_tester/src/analog_menu.py
@@ -10,10 +10,15 @@ class AnalogMenu(Menu):
         ADC.setup()
 
     def show_text(self):
-        for i, jack in enumerate(self.jack_to_analog):
+        for i, jack in enumerate(self.sorted_jacks()):
             self.screen.addstr(i, 0, (jack) + ': ' + str(ADC.read(self.jack_to_analog[jack])))
 
         self.show_quit(len(self.jack_to_analog)+1)
+
+
+    def sorted_jacks(self):
+        numbers = sorted(int(j[1:] for j in self.jack_to_analog.keys())
+        return ['J' + str(n) for n in numbers]
 
     def delay(self):
         return ANALOG_MENU_DELAY_MS
