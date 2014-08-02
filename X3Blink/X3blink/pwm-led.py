a1cce75876d2f7e7cd405e3b9b79ceaabc2e90c9
Doug Gilliland <doug@douglasgilliland.com>
2014-08-02 14:14:35 -0400

    PWM code

--- X3Blink/X3blink/pwm-led.py
+++ X3Blink/X3blink/pwm-led.py
@@ -2,25 +2,8 @@ import Adafruit_BBIO.PWM as PWM
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-J8P3 = "GPIO3_16"	# ground pin is at wrong end
-J9P3 = "GPIO0_15"	# UART1 Tx
-J9P4 = "GPIO0_14"	# UART1 Rx
-J11 = "GPIO1_17"
-J12P3 = "GPIO0_3"	# UART2 Tx
-J12P4 = "GPIO0_2"	# UART2 Rx
-J13P3 = "GPIO0_4"	# I2C 1 SDA
-J13P4 = "GPIO0_5"	# I2C 1 SCL
-J14 = "GPIO1_16"
-J15 = "GPIO1_28"
 J19 = "GPIO0_22"	# EHRPWM2A
-J21 = "GPIO0_27"
-J22 = "GPIO2_1"
-J23 = "GPIO1_15"
 J24 = "GPIO0_23"	# EHRPWM2B
-J25 = "GPIO2_4"		# TIMER6
-J26 = "GPIO2_5"		# TIMER5
-J27 = "GPIO2_3"		# TIMER7
-J28 = "GPIO2_2"		# TIMER4
 J37 = "GPIO1_29"
 	
 dutyCycle = 0.0
