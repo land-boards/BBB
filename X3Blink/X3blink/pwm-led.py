8e42b2993599bde96dbfc1a75e43261c0b8c33c1
Doug Gilliland <doug@douglasgilliland.com>
2014-08-03 19:06:34 -0400

    fygghh

--- X3Blink/X3blink/pwm-led.py
+++ X3Blink/X3blink/pwm-led.py
@@ -6,6 +6,14 @@ J25 = 'P8_13'
 
 while True:
 	dutyCycle = 0.0
+	PWM.start(J25, dutyCycle, 1000, 1)
+	time.sleep(0.1)
+	while dutyCycle < 100.0:
+		PWM.set_duty_cycle(J25, dutyCycle)
+		time.sleep(0.1)
+		dutyCycle += 5.0
+	PWM.stop(J25)
+	dutyCycle = 0.0
 #	PWM.start('P8_19', dutyCycle, 1000, 1)
 	PWM.start('P8_19')
 	time.sleep(0.1)
@@ -15,13 +23,5 @@ while True:
 		time.sleep(0.1)
 		dutyCycle += 5.0
 	PWM.stop('P8_19')
-	dutyCycle = 0.0
-	PWM.start(J25, dutyCycle, 1000, 1)
-	time.sleep(0.1)
-	while dutyCycle < 100.0:
-		PWM.set_duty_cycle(J25, dutyCycle)
-		time.sleep(0.1)
-		dutyCycle += 5.0
-	PWM.stop(J25)
 
 PWM.cleanup()
