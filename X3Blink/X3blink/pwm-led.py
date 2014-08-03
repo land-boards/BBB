426a8d1255f26571936f1989cab089b8a732bf6e
Doug Gilliland <doug@douglasgilliland.com>
2014-08-03 16:16:19 -0400

    two chs

--- X3Blink/X3blink/pwm-led.py
+++ X3Blink/X3blink/pwm-led.py
@@ -4,19 +4,21 @@ import time
 J18 = 'P8_19'
 J25 = 'P8_13'
 
-dutyCycle = 0.0
-
 #optionally, you can set the frequency as well as the polarity from their defaults:
-PWM.start(J18, 100.0 - dutyCycle, 1000, 1)
+PWM.start(J18, dutyCycle, 1000, 1)
 PWM.start(J25, dutyCycle, 1000, 1)
 
 while True:
-	PWM.set_duty_cycle(J18, dutyCycle)
-	PWM.set_duty_cycle(J25, 100.0 - dutyCycle)
-	time.sleep(0.1)
-	dutyCycle += 5.0
-	if dutyCycle > 100.0:
-		dutyCycle = 0
+	dutyCycle = 0.0
+	while dutyCycle < 100.0:
+		PWM.set_duty_cycle(J18, dutyCycle)
+		time.sleep(0.1)
+		dutyCycle += 5.0
+	dutyCycle = 0.0
+	while dutyCycle < 100.0:
+		PWM.set_duty_cycle(J25, dutyCycle)
+		time.sleep(0.1)
+		dutyCycle += 5.0
 
 PWM.stop(J18)
 PWM.stop(J25)
