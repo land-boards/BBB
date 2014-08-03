c8afd9a5ac9035bd076de6666939d8333a595329
Doug Gilliland <doug@douglasgilliland.com>
2014-08-03 19:41:52 -0400

    upgraded library and works

--- X3Blink/X3blink/pwm-led.py
+++ X3Blink/X3blink/pwm-led.py
@@ -1,5 +1,5 @@
 import Adafruit_BBIO.PWM as PWM
-# import time
+ import time
 
 J18 = 'P8_19'
 J25 = 'P8_13'
@@ -16,13 +16,13 @@ while True:
 	PWM.stop('P8_19')
 	PWM.cleanup()
 	dutyCycle = 0.0
-	PWM.start('P8_19',0)
-#	PWM.start('P8_19', 0.0, 1000, 1)
-#	time.sleep(0.1)
+#	PWM.start('P8_19',0)
+	PWM.start('P8_19', 0.0, 1000, 1)
+	time.sleep(0.1)
 	print 'thru'
 	while dutyCycle < 100.0:
 		PWM.set_duty_cycle('P8_19', dutyCycle)
-#		time.sleep(0.1)
+		time.sleep(0.1)
 		dutyCycle += 5.0
 	PWM.stop('P8_19')
 
