52cfdaed313e34fdb1de68e88a9f6b75e733d8f7
Doug Gilliland <doug@douglasgilliland.com>
2014-08-03 16:11:47 -0400

    bug found try 2 channels

--- X3Blink/X3blink/pwm-led.py
+++ X3Blink/X3blink/pwm-led.py
@@ -7,10 +7,12 @@ J25 = 'P8_13'
 dutyCycle = 0.0
 
 #optionally, you can set the frequency as well as the polarity from their defaults:
+PWM.start(J18, 100.0 - dutyCycle, 1000, 1)
 PWM.start(J25, dutyCycle, 1000, 1)
 
 while True:
 	PWM.set_duty_cycle(J25, dutyCycle)
+	PWM.set_duty_cycle(J25, 100.0 - dutyCycle)
 	time.sleep(0.1)
 	dutyCycle += 5.0
 	if dutyCycle > 100.0:
