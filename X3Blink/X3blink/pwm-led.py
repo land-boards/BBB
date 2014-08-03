3530cff8aad2dcf9fd054fb712b0f9135657cad9
Doug Gilliland <doug@douglasgilliland.com>
2014-08-03 15:48:39 -0400

    added 2nd pwm

--- X3Blink/X3blink/pwm-led.py
+++ X3Blink/X3blink/pwm-led.py
@@ -2,17 +2,20 @@ import Adafruit_BBIO.PWM as PWM
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
+J18 = 'P8_19'
+J25 = 'P8_13'
+
 dutyCycle = 0.0
 
 #optionally, you can set the frequency as well as the polarity from their defaults:
-PWM.start("P8_19", dutyCycle, 1000, 1)
+PWM.start(J18, dutyCycle, 1000, 1)
 
 while True:
-	PWM.set_duty_cycle("P8_19", dutyCycle)
+	PWM.set_duty_cycle(J18, dutyCycle)
 	time.sleep(0.1)
 	dutyCycle += 5.0
 	if dutyCycle > 100.0:
 		dutyCycle = 0
 
-PWM.stop("P8_19")
+PWM.stop(J18)
 PWM.cleanup()
