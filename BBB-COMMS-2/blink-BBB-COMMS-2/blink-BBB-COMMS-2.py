63928eff9ed6ad0e95a46562d0ef4bd950863257
Doug Gilliland <doug@douglasgilliland.com>
2017-07-20 18:38:29 -0400

    use led numbers

--- BBB-COMMS-2/blink-BBB-COMMS-2/blink-BBB-COMMS-2.py
+++ BBB-COMMS-2/blink-BBB-COMMS-2/blink-BBB-COMMS-2.py
@@ -4,28 +4,26 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-H3_3 = "P9_27"
-H3_4 = "P9_23"
-H4_3 = "P9_24"
-H4_4 = "P9_26"
-H5_3 = "P9_21"
-H5_4 = "P9_22"
-H6_3 = "P9_18"
-H6_4 = "P9_17"
-H7_3 = "P9_16"
-H7_4 = "P9_15"
-H8_3 = "P9_14"
-H8_4 = "P9_12"
-H9_3 = "P9_13"
-H9_4 = "P9_11"
+D0 = "P8_26"
+D1 = "P8_17"
+D2 = "P8_15"
+D3 = "P8_11"
+D4 = "P9_23"
+D5 = "P9_26"
+D6 = "P9_22"
+D7 = "P9_15"
+D8 = "P9_12"
+D9 = "P9_11"
 
-H11 = "P8_26"
-H12 = "P8_18"
-H13 = "P8_17"
-H14 = "P8_16"
-H15 = "P8_15"
-H16 = "P8_12"
-H17 = "P8_11"
+D16 = "P8_18"
+D17 = "P8_16"
+D18 = "P8_12"
+D19 = "P9_27"
+D20 = "P9_24"
+D21 = "P9_21"
+D22 = "P9_16"
+D23 = "P9_14"
+D24 = "P9_13"
 
 def blinkLEDHigh(channel):
 	GPIO.output(channel, GPIO.HIGH)
@@ -37,48 +35,45 @@ def blinkLEDLow(channel):
 	time.sleep(0.25)
 	GPIO.output(channel, GPIO.HIGH)
 	
-GPIO.setup(H3_3, GPIO.OUT)
-GPIO.setup(H3_4, GPIO.OUT)
-GPIO.setup(H4_3, GPIO.OUT)
-GPIO.setup(H4_4, GPIO.OUT)
-GPIO.setup(H5_3, GPIO.OUT)
-GPIO.setup(H5_4, GPIO.OUT)
-# GPIO.setup(H6_3, GPIO.OUT)
-# GPIO.setup(H6_4, GPIO.OUT)
-GPIO.setup(H7_3, GPIO.OUT)
-GPIO.setup(H7_4, GPIO.OUT)
-GPIO.setup(H8_3, GPIO.OUT)
-GPIO.setup(H8_4, GPIO.OUT)
-GPIO.setup(H9_3, GPIO.OUT)
-GPIO.setup(H9_4, GPIO.OUT)
-
-GPIO.setup(H11, GPIO.OUT)
-GPIO.setup(H12, GPIO.OUT)
-GPIO.setup(H13, GPIO.OUT)
-GPIO.setup(H14, GPIO.OUT)
-GPIO.setup(H15, GPIO.OUT)
-GPIO.setup(H16, GPIO.OUT)
-GPIO.setup(H17, GPIO.OUT)
+GPIO.setup(D0, GPIO.OUT)
+GPIO.setup(D1, GPIO.OUT)
+GPIO.setup(D2, GPIO.OUT)
+GPIO.setup(D3, GPIO.OUT)
+GPIO.setup(D4, GPIO.OUT)
+GPIO.setup(D5, GPIO.OUT)
+GPIO.setup(D6, GPIO.OUT)
+GPIO.setup(D7, GPIO.OUT)
+GPIO.setup(D8, GPIO.OUT)
+GPIO.setup(D9, GPIO.OUT)
+GPIO.setup(D16, GPIO.OUT)
+GPIO.setup(D17, GPIO.OUT)
+GPIO.setup(D18, GPIO.OUT)
+GPIO.setup(D19, GPIO.OUT)
+GPIO.setup(D20, GPIO.OUT)
+GPIO.setup(D21, GPIO.OUT)
+GPIO.setup(D22, GPIO.OUT)
+GPIO.setup(D23, GPIO.OUT)
+GPIO.setup(D24, GPIO.OUT)
 
 while True:
-	blinkLEDHigh(H11)
-	blinkLEDHigh(H13)
-	blinkLEDHigh(H15)
-	blinkLEDHigh(H17)
-	blinkLEDHigh(H16)
-	blinkLEDHigh(H14)
-	blinkLEDHigh(H12)
-	blinkLEDHigh(H3_3)
-	blinkLEDHigh(H4_3)
-	blinkLEDHigh(H5_3)
+	blinkLEDHigh(D0)
+	blinkLEDHigh(D1)
+	blinkLEDHigh(D2)
+	blinkLEDHigh(D3)
+	blinkLEDHigh(D4)
+	blinkLEDHigh(D5)
+	blinkLEDHigh(D6)
+	blinkLEDHigh(D7)
+	blinkLEDHigh(D8)
+	blinkLEDHigh(D9)
+	blinkLEDHigh(D16)
+	blinkLEDHigh(D17)
+	blinkLEDHigh(D18)
+	blinkLEDHigh(D19)
+	blinkLEDHigh(D20)
+	blinkLEDHigh(D21)
+	blinkLEDHigh(D22)
+	blinkLEDHigh(D23)
+	blinkLEDHigh(D24)
 	# blinkLEDHigh(H6_3)
 	# blinkLEDHigh(H6_4)
-	blinkLEDHigh(H7_3)
-	blinkLEDHigh(H8_3)
-	blinkLEDHigh(H9_3)
-	blinkLEDHigh(H9_4)
-	blinkLEDHigh(H8_4)
-	blinkLEDHigh(H7_4)
-	blinkLEDHigh(H5_4)
-	blinkLEDHigh(H4_4)
-	blinkLEDHigh(H3_4)
