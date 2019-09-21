a9241490b67691b99aa5b7d8bc79ad0ca67a38a3
root <root@beaglebone.(none)>
2019-09-21 11:03:41 -0400

    loosen analog tolerances a small amount

--- BBB-GVS-3/analogLadder/analogLadder.py
+++ BBB-GVS-3/analogLadder/analogLadder.py
@@ -18,25 +18,25 @@ print 'val6 ',val6
 
 errors = False
 
-if val0 < 0.61 or val0 > 0.63:
+if val0 < 0.605 or val0 > 0.645:
 	print 'error in val1'
 	errors = True
-if val1 < 0.12 or val1 > 0.13:
+if val1 < 0.105 or val1 > 0.145:
 	print 'error in val1'
 	errors = True
-if val2 < 0.74 or val2 > 0.76:
+if val2 < 0.73 or val2 > 0.77:
 	print 'error in val1'
 	errors = True
-if val3 < 0.24 or val3 > 0.26:
+if val3 < 0.23 or val3 > 0.27:
 	print 'error in val1'
 	errors = True
-if val4 < 0.49 or val4 > 0.51:
+if val4 < 0.48 or val4 > 0.53:
 	print 'error in val1'
 	errors = True
-if val5 < 0.37 or val5 > 0.38:
+if val5 < 0.355 or val5 > 0.395:
 	print 'error in val1'
 	errors = True
-if val6 < 0.87 or val6 > 0.88:
+if val6 < 0.855 or val6 > 0.895:
 	print 'error in val1'
 	errors = True
 if errors == True:
